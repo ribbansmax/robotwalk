71d32ead6288af1ae85eac69efe01871ce513646
Max Ribbans <ribbans.max@gmail.com>
2021-04-07 11:18:25 -0400

    Add method to check if robot is bounded after repeating the command forever

--- robot.rb
+++ robot.rb
@@ -1,20 +1,31 @@
-def return?(string)
-  # position indexes are for N, E, S, W
-  position = [0, 0, 0, 0]
-  direction = 0
-  until string.length == 0
-    command = string.slice!(0)
-    if command == 'g'
-      position[direction] +=1
-    elsif command == 'r'
-      direction = (direction + 1) % 4
-    else
-      direction = (direction - 1) % 4
+class Robot
+  def return?(string)
+    # position indexes are for N, E, S, W
+    position = [0, 0, 0, 0]
+    direction = 0
+    until string.length == 0
+      command = string.slice!(0)
+      if command == 'g'
+        position[direction] +=1
+      elsif command == 'r'
+        direction = (direction + 1) % 4
+      else
+        direction = (direction - 1) % 4
+      end
     end
+    (position[0] == position[2]) && (position[1] == position[3])
   end
-  (position[0] == position[2]) && (position[1] == position[3])
+
+  def bounded?(string)
+    direction = 0
+    until string.length == 0
+      command = string.slice!(0)
+      if command == 'r'
+        direction = (direction + 1) % 4
+      else
+        direction = (direction - 1) % 4
+      end
+    end
+    direction != 0
+  end
 end
-
-require 'pry';binding.pry
-1
-
